77f32c8b07230ff2b87365dbbb7b6514d5d7ad89
BennetXr <144207747+BennetXr@users.noreply.github.com>
2023-11-14 10:43:26 +0400

    exe2.py

---
+++ Chapter 4- Control Flow/Exercises/exe2.py
@@ -0,0 +1,13 @@
+#if 
+alien_color = 'red'
+if alien_color == 'green':
+    print("You just earned 5 points")
+else:
+    print("You just earned 10 points")
+
+#else 
+alien_color = 'red'
+if alien_color == 'green':
+    print("You just earned 5 points")
+else:
+    print("You just earned 10 points")

45ecdcebc1f68d92a894b06065aaabc78b2a0ba4
BennetXr <144207747+BennetXr@users.noreply.github.com>
2023-11-14 10:32:55 +0400

    exe2.py

---
+++ Chapter 2- Variables & Comments/Exercises/exe2.py
@@ -0,0 +1,3 @@
+#Find a quote from a famous person you admire. Print the quote and the name of its author.
+print('Walt Disney once said, "The way to get started is to quit talking')
+print('and start doing,"')

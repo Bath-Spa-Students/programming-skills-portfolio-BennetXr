577d32581d8f475ab6a9af194488783b278d6451
BennetXr <144207747+BennetXr@users.noreply.github.com>
2023-11-14 10:37:50 +0400

    Create exe4.py

---
+++ Chapter 3- Data Structures/Exercises/exe4.py
@@ -0,0 +1,8 @@
+#invite peopele to dinner
+guests=["jozef","bennet","jyothir"]
+name= guests[0].title()
+print(name + ", please come to dinner.")
+name= guests[1].title()
+print(name + ", please come to dinner.")
+name= guests[2].title()
+print(name + ", please come to dinner.")
